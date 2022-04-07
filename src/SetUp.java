bbfed8048b62a8cbf632f8b4372f262924d78608
ferhattongel <ferhattongel@gmail.com>
2022-04-06 22:28:58 -0400

    Ferhat made more changes

--- src/SetUp.java
+++ src/SetUp.java
@@ -1,10 +1,6 @@
 public class SetUp {
     //setting up project for everyone
 
-    //Erdi made a branch and edited this file
-
-    //made some more changes
-
     // Ferhat made a changes and edited this file
 
     // Ferhat made more changes
